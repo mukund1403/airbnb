e0af2012b9498d9d3b71d0de70a6dd03038985cc
mukund1403 <108778858+mukund1403@users.noreply.github.com>
2023-01-02 12:08:51 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id: 1,
+        title: "Camel riding in Dubai",
+        description: "Enjoy an amazing camel-riding experience during sunset in the sand dunes of DUbai",
+        price: 100,
+        coverImg: "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/36/92/e5.jpg",
+        stats: {
+            rating: 5.1,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn cooking with a professional chef",
+        description: "Learn how to cook Italian food taught by a professional chef, at the comfort of your own home",
+        price: 200,
+        coverImg: "https://media.30seconds.com/tip/lg/Knife-Tips-From-Chefs-3-Ways-to-Save-Your-Knives-While-Coo-22573-0c873fd071-1619459903.jpg",
+        stats: {
+            rating: 5.5,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Private flying lessons",
+        description: "Ever wanted to learn how to comandeer a plane by yourself? Now you can! With Singapore's first private flying school.",
+        price: 500,
+        coverImg: "https://i2-prod.mirror.co.uk/incoming/article20707717.ece/ALTERNATES/s615b/0_THP_CHP_231019SLUG_6908JPG.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Singapore",
+        openSpots: 3,
+    }
+]
